5c6bb8aa90331c61ae45da2c673d5ebb866fe727
Matthew Eddy <meddy672@gmail.com>
2023-08-02 17:09:30 -0400

    added object properties

--- QuoteEngine/PDFIngestor.py
+++ QuoteEngine/PDFIngestor.py
@@ -3,5 +3,7 @@ from .IngestorInterface import IngestorInterface
 class PDFIngestor(IngestorInterface):
     """Stratgey object for docx files."""
 
+    allowed_extensions = ['pdf']
+
     def __init__(self) -> None:
         super().__init__()
