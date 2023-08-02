62a22725872c7094e3fa5d06b1a10fcde0e0a46d
Matthew Eddy <meddy672@gmail.com>
2023-08-02 17:08:05 -0400

    added object property

--- QuoteEngine/DOCXIngestor.py
+++ QuoteEngine/DOCXIngestor.py
@@ -3,5 +3,7 @@ from .IngestorInterface import IngestorInterface
 class DOCXIngestor(IngestorInterface):
     """Strategy object for docx files."""
 
+    allowed_extensions = ['docx']
+
     def __init__(self) -> None:
         super().__init__()
