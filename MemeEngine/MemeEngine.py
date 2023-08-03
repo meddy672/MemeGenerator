7f8a658cab580c52f91be846f02c0e7d5ae7368c
Matthew Eddy <meddy672@gmail.com>
2023-08-03 16:02:31 -0400

    updated meme engine

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -1,9 +1,11 @@
-"""
-"""
+"""Module for the MemeEngine."""
+from PIL import Image, ImageDraw, ImageFont
 
 class MemeEngine:
     """Model for generating memes."""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, output_dir='./out_img') -> None:
+        """Instantiate object."""
+        self.out_dir = output_dir 
+    
     
