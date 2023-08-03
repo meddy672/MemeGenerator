02e519efecf39c9c6f8a05636283e25d1a449d30
Matthew Eddy <meddy672@gmail.com>
2023-08-02 20:12:14 -0400

    added ingestor class

---
+++ QuoteEngine/Ingestor.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from QuoteEngine.QuoteModel import QuoteModel
+
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+from .DOCXIngestor import DOCXIngestor
+from .CSVIngestor import CSVIngestor
+from .PDFIngestor import PDFIngestor
+from .TXTIngestor import TXTIngestor
+
+class Ingestor(IngestorInterface):
+    """Encapsulate Ingestor classes and executes ingestor by file type."""
+
+    ingestors = [DOCXIngestor, CSVIngestor, PDFIngestor, TXTIngestor]
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        pass
